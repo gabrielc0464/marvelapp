d5a0b65cdbc7e0c3bccc82eba08ef69c2a2a545b
Gabriel Charriez <gabrielc0464@hstat.org>
2017-03-20 02:05:16 +0000

    api config

--- models/model.rb
+++ models/model.rb
@@ -1,7 +1,15 @@
 require 'net/http'
 require 'pp'
 require 'json'
-url = 'http://gateway.marvel.com/v1/public/comics?issueNumber=1'
-uri= URI(url)
-response= Net::HTTP.get(url)
-json.parse(response)
+require "marvel_api"
+# url = 'http://gateway.marvel.com/v1/public/characters'
+# uri= URI(url)
+# response= Net::HTTP.get(url)
+# json.parse(response)
+# @client = Marvel::Client.new
+
+# @client.configure do |config|
+#   config.api_key = '74b98ca15d87e5539c26ee13a2ba0f69'
+#   config.private_key = '63db98dc1123d3b82d53beadd03dcb47b37268d2'
+# end
+# https://gateway.marvel.com:443/v1/public/characters?apikey=74b98ca15d87e5539c26ee13a2ba0f69    #link to characters hash
